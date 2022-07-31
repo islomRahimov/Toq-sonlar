12f7b357e7b00de02bae57876b18898b542356b8
islomRahimov <110152566+islomRahimov@users.noreply.github.com>
2022-07-31 11:46:43 +0500

    Add files via upload

---
+++ untitled6/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+  int sd = 1;
+  do {
+      if (sd % 2 == 1)
+          System.out.println("Toq sonlar: "+ sd);
+      sd++;
+  }
+while (sd <= 20);
+    }
+}
